711604bb6d1686f0e601f015a5a775715b79f5cd
Btuturice <btuturice@outlook.com>
2016-03-23 11:08:29 -0400

    rt

--- ZombiePong/ZombiePong/ZombiePong/Game1.cs
+++ ZombiePong/ZombiePong/ZombiePong/Game1.cs
@@ -8,7 +8,6 @@ using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Media;
-
 namespace ZombiePong
 {
     /// <summary>
@@ -19,34 +18,28 @@ namespace ZombiePong
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
         Texture2D background, spritesheet;
-
         Sprite paddle1, paddle2, ball;
-
+        Random rand = new Random();
         List<Sprite> zombies = new List<Sprite>();
-
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
             Content.RootDirectory = "Content";
-
             graphics.PreferredBackBufferWidth = 1024;
             graphics.PreferredBackBufferHeight = 768;
             graphics.ApplyChanges();
         }
-
         /// <summary>
         /// Allows the game to perform any initialization it needs to before starting to run.
         /// This is where it can query for any required services and load any non-graphic
-        /// related content.  Calling base.Initialize will enumerate through any components
+        /// related content. Calling base.Initialize will enumerate through any components
         /// and initialize them as well.
         /// </summary>
         protected override void Initialize()
         {
             // TODO: Add your initialization logic here
-
             base.Initialize();
         }
-
         /// <summary>
         /// LoadContent will be called once per game and is the place to load
         /// all of your content.
@@ -55,17 +48,13 @@ namespace ZombiePong
         {
             // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
             background = Content.Load<Texture2D>("background");
             spritesheet = Content.Load<Texture2D>("spritesheet");
-
             paddle1 = new Sprite(new Vector2(20, 20), spritesheet, new Rectangle(0, 516, 25, 150), Vector2.Zero);
             paddle2 = new Sprite(new Vector2(970, 20), spritesheet, new Rectangle(32, 516, 25, 150), Vector2.Zero);
-            ball = new Sprite(new Vector2(700, 350), spritesheet, new Rectangle(76, 510, 40, 40), new Vector2(500, 120));
-
+            ball = new Sprite(new Vector2(700, 350), spritesheet, new Rectangle(76, 510, 40, 40), new Vector2(-300, 0));
             SpawnZombie(new Vector2(400, 400), new Vector2(-20, 0));
         }
-
         /// <summary>
         /// UnloadContent will be called once per game and is the place to unload
         /// all content.
@@ -74,19 +63,15 @@ namespace ZombiePong
         {
             // TODO: Unload any non ContentManager content here
         }
-
         public void SpawnZombie(Vector2 location, Vector2 velocity)
         {
             Sprite zombie = new Sprite(location, spritesheet, new Rectangle(0, 25, 160, 150), velocity);
-
             for (int i = 1; i < 10; i++)
             {
                 zombie.AddFrame(new Rectangle(i * 165, 25, 160, 150));
             }
-
             zombies.Add(zombie);
         }
-
         /// <summary>
         /// Allows the game to run logic such as updating the world,
         /// checking for collisions, gathering input, and playing audio.
@@ -97,41 +82,42 @@ namespace ZombiePong
             // Allows the game to exit
             if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed)
                 this.Exit();
+            MouseState ms = Mouse.GetState();
+            paddle1.Location = new Vector2(paddle1.Location.X, ms.Y);
 
-            // TODO: Add your update logic here
+            // Ball can't go past screen
+            if (ball.Location.X < -20)
+                ball.Location = new Vector2(700, 350);
+            if (ball.Location.X > 1044)
+                ball.Location = new Vector2(400, 350);
+            if (ball.Location.Y >= 1008)
+                ball.Velocity = new Vector2(ball.Velocity.X, ball.Velocity.Y * -1);
+            if (ball.Location.Y <= 0)
+                ball.Velocity = new Vector2(ball.Velocity.X, ball.Velocity.Y * -1);
+
+            // P2 AI
+            paddle2.Location = new Vector2(paddle2.Location.X, ball.Center.Y - 60);
+
+            // Collision code
+            if (ball.IsBoxColliding(paddle1.BoundingBoxRect) && ball.Location.Y != paddle1.Center.Y)
+            {
+                ball.Velocity = new Vector2(ball.Velocity.X * -1.05f, (float)Math.Sin(ball.Location.Y - paddle1.Center.Y) * -50);
+            }
+            if (ball.IsBoxColliding(paddle2.BoundingBoxRect) && ball.Location.Y != paddle2.Center.Y)
+            {
+                ball.Velocity = new Vector2(ball.Velocity.X * -1.05f, (float)Math.Sin(ball.Location.Y - paddle2.Center.Y) * -50);
+            }
+
             ball.Update(gameTime);
-            MouseState MS = Mouse.GetState();
-            paddle1.Location = new Vector2(paddle1.Location.X, MS.Y);
-            paddle2.Location = new Vector2(paddle2.Location.X, ball.Center.Y - 90);
-            if (ball.Location.Y > 750 - 16 || ball.Location.Y < 0 + 5)
-                ball.Velocity = ball.Velocity * new Vector2(1, -1);
-            //if (ball.Location.X > 950 - 16 || ball.Location.X < 0 + 20)
-            //    ball.Velocity = ball.Velocity * new Vector2(-1, 1);
-
-            if (ball.BoundingBoxRect.Left < paddle1.BoundingBoxRect.Right && 
-                ball.BoundingBoxRect.Bottom < paddle1.BoundingBoxRect.Top &&
-                ball.BoundingBoxRect.Top > paddle1.BoundingBoxRect.Bottom
-                )
-                ball.Velocity = ball.Velocity * new Vector2(-1, 1);
-
-            if (ball.BoundingBoxRect.Left < paddle2.BoundingBoxRect.Right &&
-                ball.BoundingBoxRect.Bottom < paddle2.BoundingBoxRect.Top &&
-                ball.BoundingBoxRect.Top > paddle2.BoundingBoxRect.Bottom
-                )
-                ball.Velocity = ball.Velocity * new Vector2(-1, 1);
-
 
             for (int i = 0; i < zombies.Count; i++)
             {
                 zombies[i].Update(gameTime);
-
-                // Zombie logic goes here.. 
+                // Zombie logic goes here..
                 zombies[i].FlipHorizontal = false;
             }
-
             base.Update(gameTime);
         }
-
         /// <summary>
         /// This is called when the game should draw itself.
         /// </summary>
@@ -139,22 +125,16 @@ namespace ZombiePong
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             spriteBatch.Begin();
-            
             spriteBatch.Draw(background, Vector2.Zero, Color.White);
-
             paddle1.Draw(spriteBatch);
             paddle2.Draw(spriteBatch);
             ball.Draw(spriteBatch);
-
             for (int i = 0; i < zombies.Count; i++)
             {
                 zombies[i].Draw(spriteBatch);
             }
-
             spriteBatch.End();
-
             base.Draw(gameTime);
         }
     }
